247ccc2cfc64c5803433fe0ea685c64f771a8297
ariyanto915 <49186038+ariyanto915@users.noreply.github.com>
2019-07-20 19:59:05 +0700

    Update Kalender.py

--- Kalender.py
+++ Kalender.py
@@ -14,9 +14,9 @@ kalender = [('Januari', range(1, 31+1)),
 minggu = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
 
 def klndr(tahun, awal_hari):
-    pos_awal = minggu.index(awal_hari)
+    pos_awal = minggu.index('Tu')
     
-    if kabisat(tahun):
+    if kabisat(yr):
         kalender[1] = ('Februari', range(1, 29+1))
         
     for bulan, days in kalender:
@@ -31,19 +31,20 @@ def klndr(tahun, awal_hari):
                 print()
                 pos_awal = 0
         print('\n')
-        
+
 def kabisat(tahun):
-    if tahun % 4 == 0:
-        if tahun % 100 == 0:
-            if tahun % 400 == 0:
+    if tahun%400==0:
+        return True
+    else:
+        if tahun%100==0:
+            return False
+        else:
+            if tahun%4==0:
                 return True
             else:
                 return False
-        else:
-            return False
-    else:
-        return False
 
 yr = int(input('Masukan Tahun : '))
-strtday = input('Masukan Hari (Mon, Tue, Wed, Thu, Fri, Sat, Sun) : ')
-klndr(yr, strtday)
+#strtday = input('Masukan Hari (Mon, Tue, Wed, Thu, Fri, Sat, Sun) : ')
+#strtday = Tu
+klndr(yr, minggu.index('Tu'))
